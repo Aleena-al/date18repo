421b00dfc5506b2e73848c17471387e40c0db217
Aleenaa <aleenaadavis123@gmail.com>
2023-05-18 16:01:35 +0530

    dot operator and squre

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 function main()
 {
-    let obj={};
+    
     let obj1={id:1,name:"ammu"};
-    let obj2={id:2,name:"annu",age:20};
+    
 
-    console.log(obj1);
-    console.log(obj2);
+    console.log(obj1.name);
+    
 
 }
 main();
